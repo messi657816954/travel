7382ef2cdf659240769b6fd610dd36f8b024cada
macboock. <macboock.@MacBook-Pro-de-macboock.local>
2025-01-19 00:39:31 +0100

    Ajout des logs

--- conf/settings.py
+++ conf/settings.py
@@ -202,3 +202,18 @@ SIMPLE_JWT = {
     # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
