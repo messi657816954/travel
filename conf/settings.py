66854049211370ca41eeff9f448d6237215a6e4a
guyzo <mbunto2@gmail.com>
2025-02-03 22:50:55 +0100

    Swagger

--- conf/settings.py
+++ conf/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'reservations',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',  # Pour inclure Swagger UI et ReDoc
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -159,6 +162,8 @@ REST_FRAMEWORK = {
         
         'login': '2/day'
     },
+
+    'DEFAULT_SCHEMA_CLASS' : 'drf_spectacular.openapi.AutoSchema',
     
 }
 
--- conf/urls.py
+++ conf/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
@@ -9,6 +10,9 @@ urlpatterns = [
     path('ws/api/v1/annonce/', include('annonces.api.urls')),
     path('ws/api/v1/reservation/', include('reservations.api.urls')),
     path('ws/api/v1/referentiel/', include('commons.api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     
 ]
