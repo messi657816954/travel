4a147721022ba499db251155c008f076045e9946
guyzo <mbunto2@gmail.com>
2025-02-19 20:41:40 +0100

    --amend

--- reservations/api/serializers.py
+++ reservations/api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from annonces.models import Reservation
+from annonces.api.serializers import AnnonceDetailSerializer
 
 
 class ReservationSerializer(serializers.ModelSerializer):
+    annonce_details = AnnonceDetailSerializer(source="annonce", read_only=True)
     class Meta:
         model = Reservation
         fields = '__all__'
