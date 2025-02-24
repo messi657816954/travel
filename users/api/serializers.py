6de494584e527d3e11656be3c4de0d1301058d19
guyzo <mbunto2@gmail.com>
2025-02-24 22:30:49 +0100

    --amend

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -34,7 +34,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
                 'iso_code3': user.pays.iso_code3,
                 'label': user.pays.label,
                 'label_en': user.pays.label_en,
-                'currency': Null,
+                'currency': 0,
                 'digit_code': user.pays.digit_code
             },
             # Ajoutez d'autres champs de l'utilisateur si nécessaire
