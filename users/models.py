8750362d1f620965539433a6adbe0b3b452bd85c
guyzo <mbunto2@gmail.com>
2025-03-22 10:51:54 +0100

    --amend

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from django.db.models import Sum, Case, When, DecimalField, F, Q, Avg
+from django.db.models import Sum, Case, When, DecimalField, F, Q, Avg, Count
 
 from commons.models import Pays
 
