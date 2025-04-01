ac109b8b211489911f45caccba5ef20d42a9326d
guyzo <mbunto2@gmail.com>
2025-04-01 22:29:14 +0200

    --amend

--- conf/settings.py
+++ conf/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "media/profile_pics"),
+    os.path.join(BASE_DIR, "static/"),
 ]
 AUTH_USER_MODEL = 'users.User'
 
